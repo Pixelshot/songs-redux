190544398393b02e93be40a70b56031a51c851c2
Pixelshot <pixelshot8@gmail.com>
2021-11-20 09:43:50 +0800

    241: mapStateToProps

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,9 @@ export const selectSong = (song) => {
   return {
     // type is just name of the action
     type: "SONG_SELECTED",
+    // Payload is a non-official, community accepted (de facto) naming convention for the property that holds the actual data in a Redux action object.
+    // Payload is what is keyed ( the key value pairs ) in your actions and passed around between reducers in your redux application.
+    // From https://stackoverflow.com/questions/51357412/what-is-a-payload-in-redux-context
     payload: song,
   };
 };
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 class Songlist extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1>SongList</h1>
@@ -12,4 +13,12 @@ class Songlist extends Component {
   }
 }
 
-export default connect()(Songlist);
+// connecting React with Redux
+// mapStateToProps will always return a state that contains combineReducers
+// Technically we can destructure state but for the sake of clarity/readability we'll keep at is.
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(Songlist);
