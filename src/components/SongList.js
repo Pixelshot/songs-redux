54f2c2286b175a5b665c3dfa68f821b0e8cc340a
Pixelshot <pixelshot8@gmail.com>
2021-11-20 14:06:42 +0800

    244: Action Creators

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions/";
 
 class Songlist extends Component {
   render() {
-    console.log(this.props);
-    return (
-      <div>
-        <h1>SongList</h1>
-        <h2>SongList</h2>
-      </div>
-    );
+    // console.log(this.props);
+    const { songs, selectSong } = this.props;
+    const renderList = () => {
+      return songs.map((song) => {
+        return (
+          <div className="item" key={song.title}>
+            <div className="right floated content">
+              <button
+                className="ui button primary"
+                onClick={() => selectSong(song)}
+              >
+                Select
+              </button>
+            </div>
+
+            <div className="content">{song.title}</div>
+          </div>
+        );
+      });
+    };
+
+    return <div className="ui divided list">{renderList()}</div>;
   }
 }
 
@@ -17,8 +33,8 @@ class Songlist extends Component {
 // mapStateToProps will always return a state that contains combineReducers
 // Technically we can destructure state but for the sake of clarity/readability we'll keep at is.
 const mapStateToProps = (state) => {
-  // console.log(state);
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(Songlist);
+export default connect(mapStateToProps, { selectSong })(Songlist);
